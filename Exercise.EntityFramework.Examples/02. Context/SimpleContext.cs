f6a221c22363c39ba63eb2c6e7d6cc02884fa4d0
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 19:16:17 +0100

    Add simple context

---
+++ Exercise.EntityFramework.Examples/02. Context/SimpleContext.cs
@@ -0,0 +1,16 @@
+﻿using Exercise.EntityFramework.Examples.Model.Dependencies;
+using System.Data.Entity;
+
+namespace Exercise.EntityFramework.Examples._02._Context
+{
+    public class SimpleContext : DbContext
+    {
+        public SimpleContext() : base("SimpleContext")
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
