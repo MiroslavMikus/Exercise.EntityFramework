b04c22ed661280421caaec13bb143e2d42ae666a
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 15:54:39 +0100

    add model table name

---
+++ Exercise.EntityFramework.Examples/01. Model/02. TableName/TypeWithComplexName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Exercise.EntityFramework.Examples._01._Model._02._TableName
+{
+    /// <summary>
+    /// Attribute '[Table]' tells the Entity Framework to put this class to specified table.
+    /// </summary>
+    [Table("Simple")]
+    public class TypeWithComplexName
+    {
+        public int Id { get; set; }
+    }
+}
