04a63a6d4bea73bbde162bbb2bbf5204c66e97f5
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 15:50:47 +0100

    Add model primary key example

---
+++ Exercise.EntityFramework.Examples/01. Model/01. PrimaryKey/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise.EntityFramework.Examples.Model.PrimaryKey
+{
+    public class Customer
+    {
+        /// <summary>
+        /// Propery named 'class name'Id will be used as primary key.
+        /// This will be set by Entity Framework naming convention.
+        /// </summary>
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Exercise.EntityFramework.Examples/01. Model/01. PrimaryKey/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exercise.EntityFramework.Examples.Model
+{
+    public class Order
+    {
+        /// <summary>
+        /// Since the property OrderKey doesn't have 'Id' on the end, Entity Framework can't 
+        /// recognize it as primary key. This can be fixed with the [Key] attribute.
+        /// (or fluent api)
+        /// </summary>
+        [Key]
+        public int OrderKey { get; set; }
+    }
+}
