dee1d03757d5b58e36e6b9e703f0e1d8591dbd0e
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 15:59:50 +0100

    Add composite key example

---
+++ Exercise.EntityFramework.Examples/01. Model/01. PrimaryKey/UserEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Exercise.EntityFramework.Examples._01._Model._01._PrimaryKey
+{
+    /// <summary>
+    /// Class with composite key.
+    /// </summary>
+    public class UserEntry
+    {
+        [Key, Column(Order = 0)]
+        public string UserName { get; set; }
+
+        [Key, Column(Order = 1)]
+        public string Domain { get; set; }
+    }
+}
