3d61153c2b1eafdcea1781d88eb760d45fd5e124
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 19:51:43 +0100

    Add simple repository

---
+++ Exercise.EntityFramework.Patterns/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Exercise.EntityFramework.Patterns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise.EntityFramework.Patterns")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d8b7b7b-37e1-43c1-aea6-2413d94911d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Exercise.EntityFramework.Patterns/Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.EntityFramework.Patterns.Repository
+{
+    public class Repository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> All() => _dbSet;
+
+        public T Find(int id) => _dbSet.Find(id);
+        public T Insert(T entity) => _dbSet.Add(entity);
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(int id) => _dbSet.Remove(_dbSet.Find(id));
+    }
+}
