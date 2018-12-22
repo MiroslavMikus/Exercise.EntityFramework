a293fb7fd1ccd8849a73768a5c2915446f7e4927
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-12-22 15:42:13 +0100

    Add examples project

---
+++ Exercise.EntityFramework.Examples/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise.EntityFramework.Examples
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
