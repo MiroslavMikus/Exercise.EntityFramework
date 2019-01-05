399b237247814df551f9162b1e0132d38a37fb27
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-05 15:38:38 +0100

    Add example usage of repository

---
+++ Exercise.EntityFramework.Patterns/ExampleController.cs
@@ -0,0 +1,32 @@
+﻿using Exercise.EntityFramework.Patterns.Model;
+using Exercise.EntityFramework.Patterns.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.EntityFramework.Patterns
+{
+    /// <summary>
+    /// Example usage of your repositories in MVC Controller
+    /// </summary>
+    public class ExampleController
+    {
+        private readonly IUserRepository _userRepository;
+
+        /// <summary>
+        /// Since there is already a DIC used here -> just request the repository.
+        /// </summary>
+        /// <param name="userRepository"></param>
+        public ExampleController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> List()
+        {
+            return _userRepository.All();
+        }
+    }
+}
