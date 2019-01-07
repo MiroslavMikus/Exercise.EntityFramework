f9d1ed13efd0b97a5241f4fae12265883a9a74f2
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-07 20:32:39 +0100

    Add conventions example

---
+++ Exercise.EntityFramework.Examples/03. FluentAPI/ContextWithCustomConventions.cs
@@ -0,0 +1,38 @@
+﻿using Exercise.EntityFramework.Examples.Model.Dependencies;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Exercise.EntityFramework.Examples.FluentAPI
+{
+    public class ContextWithCustomConventions : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        /// <summary>
+        /// Here you can write your Fluent API configurations
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // all string properties max length will be 20
+            modelBuilder
+                .Properties()
+                .Where(p => p.PropertyType.Name == "String")
+                .Configure(p => p.HasMaxLength(20));
+
+            modelBuilder.Conventions.Add<PrimaryKeyConvention>();
+        }
+    }
+
+    public class PrimaryKeyConvention : Convention
+    {
+        public PrimaryKeyConvention()
+        {
+            // Property Customer_KEY and Order_KEY will be the primary key by convention!
+            Properties()
+                .Where(a => a.Name == a.DeclaringType.Name + "_KEY")
+                .Configure(a => a.IsKey());
+        }
+    }
+}
